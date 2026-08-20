43ae3f0f8f5f6084af6604242561265717b679f7
Manish Wani <manishwani03@gmail.com>
2026-08-20 23:56:16 +0530

    insert element in dom with their methods

--- 08-DOM/script.js
+++ 08-DOM/script.js
@@ -36,4 +36,40 @@ let div3 = document.querySelector("div");
 div3.style.backgroundColor = "red";
 div3.style.color = "white";
 div3.style.fontFamily = "Arial";
-div3.style.fontSize = "50px";
+div3.style.fontSize = "40px";
+
+/* 
+    insert elements
+
+    append()    - adds at the end of node (inside)
+    prepend()   - adds at the start of node (inside)
+    before()    - adds before the node (outside)
+    after()     - adds after the node (outside)
+*/
+
+let newButton = document.createElement("button");
+newButton.innerHTML = "<b><i>After Div</i></b>";
+let div4 = document.querySelector("div");
+div4.after(newButton);
+
+newButton.style.borderRadius = "5px";
+newButton.style.height = "45px";
+newButton.style.width = "120px";
+newButton.style.backgroundColor = "yellow";
+newButton.style.color = "red";
+newButton.style.fontSize = "20px";
+
+let newButton2 = document.createElement("button");
+newButton2.innerHTML = "<b><i>Prepend Div</i></b>";
+div4 = document.querySelector("div");
+div4.prepend(newButton2);
+
+let newButton3 = document.createElement("button");
+newButton3.innerHTML = "<b><i>Append Div</i></b>";
+div4 = document.querySelector("div");
+div4.append(newButton3);
+
+let newButton4 = document.createElement("button");
+newButton4.innerHTML = "<b><i>before Div</i></b>";
+div4 = document.querySelector("div");
+div4.before(newButton4);
